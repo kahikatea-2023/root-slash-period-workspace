96b6a171f477533863f919268fd511396644c534
maia-tarapipipi <tarapipipim@gmail.com>
2023-07-21 10:24:15 +1200

    install process complete

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import { Elysia, t } from "elysia";
-import { html } from "@elysiajs/html";
-import * as elements from "typed-html";
-import { db } from "./db";
-import { Todo, todos } from "./db/schema";
-import { eq } from "drizzle-orm";
+import { Elysia, t } from 'elysia'
+import { html } from '@elysiajs/html'
+import * as elements from 'typed-html'
+import { db } from './db'
+import { Todo, todos } from './db/schema'
+import { eq } from 'drizzle-orm'
 
 const app = new Elysia()
   .use(html())
-  .get("/", ({ html }) =>
+  .get('/', ({ html }) =>
     html(
       <BaseHtml>
         <body
@@ -19,25 +19,25 @@ const app = new Elysia()
       </BaseHtml>
     )
   )
-  .get("/todos", async () => {
-    const data = await db.select().from(todos).all();
-    return <TodoList todos={data} />;
+  .get('/todos', async () => {
+    const data = await db.select().from(todos).all()
+    return <TodoList todos={data} />
   })
   .post(
-    "/todos/toggle/:id",
+    '/todos/toggle/:id',
     async ({ params }) => {
       const oldTodo = await db
         .select()
         .from(todos)
         .where(eq(todos.id, params.id))
-        .get();
+        .get()
       const newTodo = await db
         .update(todos)
         .set({ completed: !oldTodo.completed })
         .where(eq(todos.id, params.id))
         .returning()
-        .get();
-      return <TodoItem {...newTodo} />;
+        .get()
+      return <TodoItem {...newTodo} />
     },
     {
       params: t.Object({
@@ -46,9 +46,9 @@ const app = new Elysia()
     }
   )
   .delete(
-    "/todos/:id",
+    '/todos/:id',
     async ({ params }) => {
-      await db.delete(todos).where(eq(todos.id, params.id)).run();
+      await db.delete(todos).where(eq(todos.id, params.id)).run()
     },
     {
       params: t.Object({
@@ -57,10 +57,10 @@ const app = new Elysia()
     }
   )
   .post(
-    "/todos",
+    '/todos',
     async ({ body }) => {
-      const newTodo = await db.insert(todos).values(body).returning().get();
-      return <TodoItem {...newTodo} />;
+      const newTodo = await db.insert(todos).values(body).returning().get()
+      return <TodoItem {...newTodo} />
     },
     {
       body: t.Object({
@@ -68,12 +68,12 @@ const app = new Elysia()
       }),
     }
   )
-  .get("/styles.css", () => Bun.file("./tailwind-gen/styles.css"))
-  .listen(3000);
+  .get('/styles.css', () => Bun.file('./tailwind-gen/styles.css'))
+  .listen(3000)
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
-);
+)
 
 const BaseHtml = ({ children }: elements.Children) => `
 <!DOCTYPE html>
@@ -82,14 +82,14 @@ const BaseHtml = ({ children }: elements.Children) => `
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>THE BETH STACK</title>
+  <title>Maia</title>
   <script src="https://unpkg.com/htmx.org@1.9.3"></script>
   <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
   <link href="/styles.css" rel="stylesheet">
 </head>
 
 ${children}
-`;
+`
 
 function TodoItem({ content, completed, id }: Todo) {
   return (
@@ -111,7 +111,7 @@ function TodoItem({ content, completed, id }: Todo) {
         X
       </button>
     </div>
-  );
+  )
 }
 
 function TodoList({ todos }: { todos: Todo[] }) {
@@ -122,7 +122,7 @@ function TodoList({ todos }: { todos: Todo[] }) {
       ))}
       <TodoForm />
     </div>
-  );
+  )
 }
 
 function TodoForm() {
@@ -136,5 +136,5 @@ function TodoForm() {
       <input type="text" name="content" class="border border-black" />
       <button type="submit">Add</button>
     </form>
-  );
+  )
 }
