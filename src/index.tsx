24d3b9ac43aa71c804939b3d4ccea2523d64dfaa
maia-tarapipipi <tarapipipim@gmail.com>
2023-07-21 16:29:44 +1200

    tile styling completed

--- src/index.tsx
+++ src/index.tsx
@@ -162,103 +162,197 @@ function HomePage() {
             <li>Privacy</li>
           </ul>
         </div>
-        <div class="w-10/12 broder-1 border-solid border-[#dcd3d7] bg-[#f7f5f6]">
+        <div class="w-10/12 border border-solid border-[#dcd3d7] bg-[#f7f5f6]">
+          {/* ----------------------------------- ROWS OF 2 ARTISTS ------------------------------------ */}
           <div>
-            <div class="text-[#ef4136] bg-[#e7e1e4] rounded-t-md font-medium m-1 p-1">
+            <div class="text-[#ef4136] bg-[#e7e1e4] rounded-t-md font-bold m-2 border border-solid border-[#dcd3d7]">
               New & Upcoming Releases
             </div>
             <div class="flex flex-row">
-              <div class="bg-[#e7e1e4] w-1/2 m-1">
-                <h2 class="text-[#ef4136] font-medium">Ryuichi Sakamoto</h2>
-                <p class="italic mb-1">12</p>
-                <p>
-                  An intimate collection of twelve compositions by Ryuichi
-                  Sakamoto, written and recorded in Tokyo during...
-                </p>
-                <p>$25.00</p>
+              <div class="bg-[#e7e1e4] w-1/2 border border-solid border-[#dcd3d7] flex m-2">
+                <img
+                  class="w-36 h-36 border border-white border-solid m-2"
+                  src="https://imgproxy.ra.co/_/quality:66/w:1500/rt:fill/aHR0cHM6Ly9zdGF0aWMucmEuY28vaW1hZ2VzL3Jldmlld3MvMjAyMy9yeXVpY2hpLXNha2Ftb3RvLTEyLXJhLXJlY29tbWVuZHMtY292ZXIuanBn"
+                  alt="Ryuichi Sakamoto"
+                />
+                <div>
+                  <h2 class="text-[#ef4136] font-medium">Ryuichi Sakamoto</h2>
+                  <p class="italic mb-1 text-sm">12</p>
+                  <p class="text-sm">
+                    An intimate collection of twelve compositions by Ryuichi
+                    Sakamoto, written and recorded in Tokyo during...
+                  </p>
+                  <p class="text-sm">$25.00</p>
+                </div>
               </div>
-              <div class="bg-[#e7e1e4] w-1/2 m-1">
-                <h2 class="text-[#ef4136] font-medium">African Head Charge</h2>
-                <p class="italic mb-1">A Trip To Bolgatanga</p>
-                <p>
-                  African Head Charge, the legendary collaboration between
-                  master percussionist Bonjo Iyabinghi Noah and...
-                </p>
-                <p>$37.00</p>
+              <div class="bg-[#e7e1e4] w-1/2 border border-solid border-[#dcd3d7] flex m-2">
+                <img
+                  class=" w-36 h-36 border border-white border-solid m-2"
+                  src="https://imgproxy.ra.co/_/quality:66/w:1500/rt:fill/aHR0cHM6Ly9zdGF0aWMucmEuY28vaW1hZ2VzL3Jldmlld3MvMjAyMy9yeXVpY2hpLXNha2Ftb3RvLTEyLXJhLXJlY29tbWVuZHMtY292ZXIuanBn"
+                  alt="Ryuichi Sakamoto"
+                />
+
+                <div>
+                  <h2 class="text-[#ef4136] font-medium">
+                    African Head Charge
+                  </h2>
+                  <p class="italic mb-1 text-sm">A Trip To Bolgatanga</p>
+                  <p class="text-sm">
+                    African Head Charge, the legendary collaboration between
+                    master percussionist Bonjo Iyabinghi Noah and...
+                  </p>
+                  <p class="text-sm">$37.00</p>
+                </div>
               </div>
-            </div>
-            <div class="flex flex-row">
-              <div class="bg-[#e7e1e4] w-1/2 m-1">
-                <h2 class="text-[#ef4136] font-medium">Ryuichi Sakamoto</h2>
-                <p class="italic mb-1">12</p>
-                <p>
-                  An intimate collection of twelve compositions by Ryuichi
-                  Sakamoto, written and recorded in Tokyo during...
-                </p>
-                <p>$25.00</p>
-              </div>
-              <div class="bg-[#e7e1e4] w-1/2 m-1">
-                <h2 class="text-[#ef4136] font-medium">African Head Charge</h2>
-                <p class="italic mb-1">A Trip To Bolgatanga</p>
-                <p>
-                  African Head Charge, the legendary collaboration between
-                  master percussionist Bonjo Iyabinghi Noah and...
-                </p>
-                <p>$37.00</p>
-              </div>
-            </div>
-            <div class="flex flex-row">
-              <div class="bg-[#e7e1e4] w-1/2 m-1">
-                <h2 class="text-[#ef4136] font-medium">Ryuichi Sakamoto</h2>
-                <p class="italic mb-1">12</p>
-                <p>
-                  An intimate collection of twelve compositions by Ryuichi
-                  Sakamoto, written and recorded in Tokyo during...
-                </p>
-                <p>$25.00</p>
-              </div>
-              <div class="bg-[#e7e1e4] w-1/2 m-1">
-                <h2 class="text-[#ef4136] font-medium">African Head Charge</h2>
-                <p class="italic mb-1">A Trip To Bolgatanga</p>
-                <p>
-                  African Head Charge, the legendary collaboration between
-                  master percussionist Bonjo Iyabinghi Noah and...
-                </p>
-                <p>$37.00</p>
-              </div>
-            </div>
-            <div class="bg-[#e7e1e4] rounded-b-md  text-[#039] hover:text-[#4d1a80] m-1 p-1">
-              » Explore More New Releases Here
             </div>
           </div>
+          {/* ----------------------------------- ROWS OF 4 ARTISTS ------------------------------------ */}
+          <div class="flex flex-row">
+            <div class="bg-[#e7e1e4] w-1/4 border border-solid border-[#dcd3d7] flex m-2">
+              <img
+                class="w-36 h-36 border border-white border-solid m-2"
+                src="https://imgproxy.ra.co/_/quality:66/w:1500/rt:fill/aHR0cHM6Ly9zdGF0aWMucmEuY28vaW1hZ2VzL3Jldmlld3MvMjAyMy9yeXVpY2hpLXNha2Ftb3RvLTEyLXJhLXJlY29tbWVuZHMtY292ZXIuanBn"
+                alt="Ryuichi Sakamoto"
+              />
+              <div class="text-sm">
+                <h2 class="text-[#ef4136] font-medium">Ryuichi Sakamoto</h2>
+                <p class="italic mb-1">12</p>
+                <p>$25.00</p>
+              </div>
+            </div>
+            <div class="bg-[#e7e1e4] w-1/4 border border-solid border-[#dcd3d7] flex m-2">
+              <img
+                class=" w-36 h-36 border border-white border-solid m-2"
+                src="https://imgproxy.ra.co/_/quality:66/w:1500/rt:fill/aHR0cHM6Ly9zdGF0aWMucmEuY28vaW1hZ2VzL3Jldmlld3MvMjAyMy9yeXVpY2hpLXNha2Ftb3RvLTEyLXJhLXJlY29tbWVuZHMtY292ZXIuanBn"
+                alt="Ryuichi Sakamoto"
+              />
+
+              <div class="text-sm">
+                <h2 class="text-[#ef4136] font-medium">African Head Charge</h2>
+                <p class="italic mb-1 ">A Trip To Bolgatanga</p>
+
+                <p>$37.00</p>
+              </div>
+            </div>
+            <div class="bg-[#e7e1e4] w-1/4 border border-solid border-[#dcd3d7] flex m-2">
+              <img
+                class=" w-36 h-36 border border-white border-solid m-2"
+                src="https://imgproxy.ra.co/_/quality:66/w:1500/rt:fill/aHR0cHM6Ly9zdGF0aWMucmEuY28vaW1hZ2VzL3Jldmlld3MvMjAyMy9yeXVpY2hpLXNha2Ftb3RvLTEyLXJhLXJlY29tbWVuZHMtY292ZXIuanBn"
+                alt="Ryuichi Sakamoto"
+              />
+              <div class="text-sm">
+                <h2 class="text-[#ef4136] font-sm">African Head Charge</h2>
+                <p class="italic mb-1">A Trip To Bolgatanga</p>
+
+                <p>$37.00</p>
+              </div>
+            </div>
+            <div class="bg-[#e7e1e4] w-1/4 border border-solid border-[#dcd3d7] flex m-2">
+              <img
+                class=" w-36 h-36 border border-white border-solid m-2"
+                src="https://imgproxy.ra.co/_/quality:66/w:1500/rt:fill/aHR0cHM6Ly9zdGF0aWMucmEuY28vaW1hZ2VzL3Jldmlld3MvMjAyMy9yeXVpY2hpLXNha2Ftb3RvLTEyLXJhLXJlY29tbWVuZHMtY292ZXIuanBn"
+                alt="Ryuichi Sakamoto"
+              />
+
+              <div class="text-sm">
+                <h2 class="text-[#ef4136] font-medium">African Head Charge</h2>
+                <p class="italic mb-1">A Trip To Bolgatanga</p>
+
+                <p>$37.00</p>
+              </div>
+            </div>
+          </div>
+
           <div>
-            <div class="text-[#ef4136] bg-[#e7e1e4] rounded-t-md font-medium m-1 p-1">
+            {/* ----------------------------------- ROWS OF 4 ARTISTS NEW STYLE ------------------------------------ */}
+            <div class="text-[#ef4136] bg-[#e7e1e4] rounded-t-md font-bold m-2 p-1  border border-solid border-[#dcd3d7]">
               Reviewed In Viva
             </div>
-            <div class="bg-[#e7e1e4] rounded-b-md text-[#039] hover:text-[#4d1a80] m-1 p-1">
+            <div>
+              <div class="flex flex-row">
+                <div class="bg-[#e7e1e4] w-1/4 border border-solid border-[#dcd3d7] flex m-2">
+                  <img
+                    class="w-36 h-36 border border-white border-solid m-2"
+                    src="https://imgproxy.ra.co/_/quality:66/w:1500/rt:fill/aHR0cHM6Ly9zdGF0aWMucmEuY28vaW1hZ2VzL3Jldmlld3MvMjAyMy9yeXVpY2hpLXNha2Ftb3RvLTEyLXJhLXJlY29tbWVuZHMtY292ZXIuanBn"
+                    alt="Ryuichi Sakamoto"
+                  />
+                  <div class="text-sm">
+                    <h2 class="text-[#ef4136] font-medium">Ryuichi Sakamoto</h2>
+                    <p class="italic mb-1">12</p>
+                    <p>$25.00</p>
+                  </div>
+                </div>
+                <div class="bg-[#e7e1e4] w-1/4 border border-solid border-[#dcd3d7] flex m-2">
+                  <img
+                    class=" w-36 h-36 border border-white border-solid m-2"
+                    src="https://imgproxy.ra.co/_/quality:66/w:1500/rt:fill/aHR0cHM6Ly9zdGF0aWMucmEuY28vaW1hZ2VzL3Jldmlld3MvMjAyMy9yeXVpY2hpLXNha2Ftb3RvLTEyLXJhLXJlY29tbWVuZHMtY292ZXIuanBn"
+                    alt="Ryuichi Sakamoto"
+                  />
+
+                  <div class="text-sm">
+                    <h2 class="text-[#ef4136] font-medium">
+                      African Head Charge
+                    </h2>
+                    <p class="italic mb-1 ">A Trip To Bolgatanga</p>
+
+                    <p>$37.00</p>
+                  </div>
+                </div>
+                <div class="bg-[#e7e1e4] w-1/4 border border-solid border-[#dcd3d7] flex m-2">
+                  <img
+                    class=" w-36 h-36 border border-white border-solid m-2"
+                    src="https://imgproxy.ra.co/_/quality:66/w:1500/rt:fill/aHR0cHM6Ly9zdGF0aWMucmEuY28vaW1hZ2VzL3Jldmlld3MvMjAyMy9yeXVpY2hpLXNha2Ftb3RvLTEyLXJhLXJlY29tbWVuZHMtY292ZXIuanBn"
+                    alt="Ryuichi Sakamoto"
+                  />
+                  <div class="text-sm">
+                    <h2 class="text-[#ef4136] font-sm">African Head Charge</h2>
+                    <p class="italic mb-1">A Trip To Bolgatanga</p>
+
+                    <p>$37.00</p>
+                  </div>
+                </div>
+                <div class="bg-[#e7e1e4] w-1/4 border border-solid border-[#dcd3d7] flex m-2">
+                  <img
+                    class=" w-36 h-36 border border-white border-solid m-2"
+                    src="https://imgproxy.ra.co/_/quality:66/w:1500/rt:fill/aHR0cHM6Ly9zdGF0aWMucmEuY28vaW1hZ2VzL3Jldmlld3MvMjAyMy9yeXVpY2hpLXNha2Ftb3RvLTEyLXJhLXJlY29tbWVuZHMtY292ZXIuanBn"
+                    alt="Ryuichi Sakamoto"
+                  />
+
+                  <div class="text-sm">
+                    <h2 class="text-[#ef4136] font-medium">
+                      African Head Charge
+                    </h2>
+                    <p class="italic mb-1">A Trip To Bolgatanga</p>
+
+                    <p>$37.00</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="bg-[#e7e1e4] rounded-b-md text-[#039] hover:text-[#4d1a80] m-1 p-1 border border-solid border-[#dcd3d7]">
               » Find Out More Here
             </div>
           </div>
           <div>
-            <div class="text-[#ef4136] bg-[#e7e1e4] rounded-t-md font-medium m-1 p-1">
+            <div class="text-[#ef4136] bg-[#e7e1e4] rounded-t-md font-bold m-1 p-1 border border-solid border-[#dcd3d7]">
               Upcoming Albums To Pre-order
             </div>
-            <div class="bg-[#e7e1e4] rounded-b-md text-[#039] hover:text-[#4d1a80] m-1 p-1">
+            <div class="bg-[#e7e1e4] rounded-b-md text-[#039] hover:text-[#4d1a80] m-1 p-1 border border-solid border-[#dcd3d7]">
               » Explore More Albums On The Horizon Here
             </div>
           </div>
           <div>
-            <div class="text-[#ef4136] bg-[#e7e1e4] rounded-t-md font-medium m-1 p-1">
+            <div class="text-[#ef4136] bg-[#e7e1e4] rounded-t-md font-bold m-1 p-1 border border-solid border-[#dcd3d7]">
               New Jazz Arrivals
             </div>
-            <div class="bg-[#e7e1e4] rounded-b-md text-[#039] hover:text-[#4d1a80] m-1 p-1">
+            <div class="bg-[#e7e1e4] rounded-b-md text-[#039] hover:text-[#4d1a80] m-1 p-1 border border-solid border-[#dcd3d7]">
               » Find more new and reissued Jazz albums here
             </div>
             <div>
-              <div class="text-[#ef4136] bg-[#e7e1e4] rounded-t-md font-medium m-1 p-1">
+              <div class="text-[#ef4136] bg-[#e7e1e4] rounded-t-md font-bold m-1 p-1 border border-solid border-[#dcd3d7]">
                 We Have Over 4000 LPs in Stock
               </div>
-              <div class="bg-[#e7e1e4] rounded-b-md p-1 m-1 text-[#039] hover:text-[#4d1a80]">
+              <div class="bg-[#e7e1e4] rounded-b-md p-1 m-1 text-[#039] hover:text-[#4d1a80] border border-solid border-[#dcd3d7]">
                 » Browse All Of Our In Stock Vinyl
               </div>
             </div>
@@ -273,22 +367,24 @@ function HomePage() {
 
 function Footer() {
   return (
-    <div>
-      <h2 class="text-[#ef4136] font-bold">Marbecks Records Est. 1934</h2>
-      <p>
-        Marbecks Records is New Zealand’s leading music specialist store selling
-        CD’s, DVD’s and Vinyl, catering not only to the connoisseurs of
-        classical music, but more to what our customers want encompassing Jazz,
-        Audio Books, World Music, International Film and interesting imports we
-        think you will adore.
-      </p>
-      <p>
-        Let’s face it… We are all passionate about music, and that’s why we’re
-        here!
-      </p>
-      <p class="text-[#039] hover:text-[#4d1a80]">
-        Read more about Marbecks Records...
-      </p>
+    <div class="flex justify-end ">
+      <div class="w-10/12 border border-solid border-[#dcd3d7] bg-[#f7f5f6] rounded-md p-1 m-1 ">
+        <h2 class="text-[#ef4136] font-bold">Marbecks Records Est. 1934</h2>
+        <p>
+          Marbecks Records is New Zealand’s leading music specialist store
+          selling CD’s, DVD’s and Vinyl, catering not only to the connoisseurs
+          of classical music, but more to what our customers want encompassing
+          Jazz, Audio Books, World Music, International Film and interesting
+          imports we think you will adore.
+        </p>
+        <p>
+          Let’s face it… We are all passionate about music, and that’s why we’re
+          here!
+        </p>
+        <p class="text-[#039] hover:text-[#4d1a80]">
+          Read more about Marbecks Records...
+        </p>
+      </div>
     </div>
   )
 }
