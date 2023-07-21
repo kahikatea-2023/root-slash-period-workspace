d6222b1d0d5642a3514ebdcb24758eb3cdd9e03f
maia-tarapipipi <tarapipipim@gmail.com>
2023-07-22 10:23:06 +1200

    code refactored into individual compontents

---
+++ src/Footer.tsx
@@ -0,0 +1,26 @@
+import * as elements from 'typed-html'
+function Footer() {
+  return (
+    <div>
+      <div class="border border-solid border-[#dcd3d7] bg-[#f7f5f6] rounded-sm p-1 m-1 ">
+        <h2 class="text-[#ef4136] font-bold">Marbecks Records Est. 1934</h2>
+        <p>
+          Marbecks Records is New Zealand’s leading music specialist store
+          selling CD’s, DVD’s and Vinyl, catering not only to the connoisseurs
+          of classical music, but more to what our customers want encompassing
+          Jazz, Audio Books, World Music, International Film and interesting
+          imports we think you will adore.
+        </p>
+        <p>
+          Let’s face it… We are all passionate about music, and that’s why we’re
+          here!
+        </p>
+        <p class="text-[#039] hover:text-[#4d1a80]">
+          Read more about Marbecks Records...
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
